47619a33e5b65e96e4384a43fe34d56a384599a0
navpreetsingh <navpreet.singh.k@gmail.com>
2014-07-02 13:18:47 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndianAnalysis::Application.config.secret_key_base = '7f95fda2d6d18a07c7a540222e575b6614cc78f77f60797942c3fd7109c43a2017ad11c4c033726e6c0e1fc12f271fa78b258d16d816f2dceb9749375ab895b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IndianAnalysis::Application.config.session_store :cookie_store, key: '_indian_analysis_session'
