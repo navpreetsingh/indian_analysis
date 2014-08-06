01311af452cfceeaf901e7d53ab1aedd50ccfe49
navpreetsingh <navpreet.singh.k@gmail.com>
2014-08-06 18:56:49 +0530

    improvements

--- app/models/nse_week_detail.rb
+++ app/models/nse_week_detail.rb
@@ -1,2 +1,10 @@
+require 'csv'
 class NseWeekDetail < ActiveRecord::Base
+
+	def self.ranking
+		week_dates = NseWeekDetail.uniq.pluck(:date1).sort[-7..-1].reverse
+		month_dates = NseMonthDetail.uniq.pluck(:date1).sort[-4..-1].reverse
+		top_stocks = NseWeekDetail.where("date1 = ?", week_dates[0]).order("oc_diff desc").limit(200)
+
+	end
 end
